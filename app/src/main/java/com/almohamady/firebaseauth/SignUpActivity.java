e321d00ebd8386939f797947ffae6b378f448c76
al Mohamady <al.Mohamady47@gmail.com>
2020-06-09 21:16:40 +0200

    FirebaseAuth For Users

--- app/src/main/java/com/almohamady/firebaseauth/SignUpActivity.java
+++ app/src/main/java/com/almohamady/firebaseauth/SignUpActivity.java
@@ -1,22 +1,35 @@
 package com.almohamady.firebaseauth;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity  extends AppCompatActivity implements View.OnClickListener {
 
+    ProgressBar progressBar;
     EditText txtUserName, txtPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.buttonSignUp).setOnClickListener(this);
         findViewById(R.id.textViewLogin).setOnClickListener(this);
+        progressBar = findViewById(R.id.progressbar);
         txtUserName = findViewById(R.id.editTextEmail);
         txtPassword = findViewById(R.id.editTextPassword);
     }
@@ -24,7 +37,32 @@ public class SignUpActivity  extends AppCompatActivity implements View.OnClickLi
     private void registerUser() {
           String userName = txtUserName.getText().toString().trim();
           String password = txtPassword.getText().toString().trim();
-          
+
+          if (userName.isEmpty()){
+              txtUserName.setError("Email is required");
+              txtUserName.requestFocus();
+          } else if (!Patterns.EMAIL_ADDRESS.matcher(userName).matches()){
+              txtUserName.setError("Email is valid");
+              txtUserName.requestFocus();
+          } else if  (password.isEmpty()) {
+              txtPassword.setError("Password is required");
+              txtPassword.requestFocus();
+          } else {
+              progressBar.setVisibility(View.VISIBLE);
+              mAuth.createUserWithEmailAndPassword(userName, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                  @Override
+                  public void onComplete(@NonNull Task<AuthResult> task) {
+                      if (task.isSuccessful()) {
+                          Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                          finish();
+                      } else {
+                          Toast.makeText(getApplicationContext(), "User Not Registered ", Toast.LENGTH_SHORT).show();
+                      }
+                      progressBar.setVisibility(View.GONE);
+                  }
+              });
+
+          }
     }
 
     @Override
